c07c63852ce16321eb7f84412a796f2c1f27885d
Rakesh <rahulrakesh16@gmail.com>
2019-11-05 11:08:13 +0530

    Changes made

--- src/test/java/testexamples/TestLogger.java
+++ src/test/java/testexamples/TestLogger.java
@@ -2,26 +2,31 @@ package testexamples;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 
-public class TestLogger  {
+public class TestLogger {
 
-    private static final Logger logger = LogManager.getLogger(TestLogger.class.getName());
-   
-    public static void main(String args[]) throws IOException {
-        LoggerContext context = (LoggerContext) LogManager.getContext(false);
-        File file = new File("./log4j2.xml");
-        context.setConfigLocation(file.toURI());
-        logger.info("Starting of For Loop");
-        for (int i = 0; i < 100; i++) {
-            logger.info("Test" + i);
-            logger.error("Test" + i);
-            logger.debug("Test" + i);
-            logger.warn("Test" + i);
-        }
-        logger.info("End of For Loop");
-    }
-} 
+	private static final Logger logger = LogManager.getLogger(TestLogger.class.getName());
+
+	public static void logs() {
+		LoggerContext context = (LoggerContext) LogManager.getContext(false);
+		File file = new File("./log4j2.xml");
+		context.setConfigLocation(file.toURI());
+		logger.info("Starting of For Loop");
+		for (int i = 0; i < 100; i++) {
+			logger.info("Test" + i);
+			logger.error("Test" + i);
+			logger.debug("Test" + i);
+			logger.warn("Test" + i);
+		}
+		logger.info("End of For Loop");
+	}
+
+	public static void main(String args[]) throws IOException {
+		logger.debug("Starting Point of Execution");
+		logs();
+		logger.debug("End  of Execution");
+	}
+}
